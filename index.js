f5342c403dc26c447c6bca9c995b0074e327545e
chrisrandall <chris.f.randall@gmail.com>
2022-05-18 08:55:38 -0600

    commit tree 9fb20bb0a9d14067ef3fd29fae88ae222402ec9b

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+findMatching = (driverNamesArray, string) => {
+    return driverNamesArray.filter(
+        (possibleMatch) => possibleMatch.toLowerCase() === string.toLowerCase()
+    );
+}
+
+fuzzyMatch = (driverNamesArray, newString) => {
+    return driverNamesArray.filter(
+        (possibleMatch) => possibleMatch.toLowerCase().indexOf(newString.toLowerCase()) === 0
+    );
+}
+// function fuzzyMatch(source, testString) {
+//     return source.filter(
+//       (possibleMatch) =>
+//         possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+//     );
+//   }
+
+function matchName(source, soughtName) {
+    return source.filter((record) => record.name === soughtName);
+  }
